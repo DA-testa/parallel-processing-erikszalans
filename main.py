e1874fa19350174d22d5e969b182e6a6c2e1da54
erikszalans <125026714+erikszalans@users.noreply.github.com>
2023-03-19 14:40:41 +0000

    cerams

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    nosleguma_laiks = [0] * n
+    thread = [[] for f in range(n)]
+
+    for i in range(m):
+        min_laiks = min(nosleguma_laiks)
+        min_n = nosleguma_laiks.index(min_laiks)
+        thread[min_n].append(i)
+
+        sakuma_laiks = max(nosleguma_laiks[min_n],0)
+        nosleguma_laiks[min_n] = sakuma_laiks + data[i]
+        output.append((min_n, sakuma_laiks))
+
 
     return output
 
@@ -15,13 +27,18 @@ def main():
     # m - job count
     n = 0
     m = 0
+    n,m = map(int, input().split())
+    
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = []
-
+    data = list(map(int, input().split()))
     # TODO: create the function
     result = parallel_processing(n,m,data)
+    
+    for i, sakuma_laiks in result:
+        print(i, sakuma_laiks)
     
     # TODO: print out the results, each pair in it's own line
 
